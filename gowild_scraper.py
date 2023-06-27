64b1e47f3b3c1382049aac7a7befddbc9b9e35a5
Hannah Burns <hannahburns787@gmail.com>
2023-06-26 21:01:51 -0500

    cleaned up some of my garbage

--- gowild_scraper.py
+++ gowild_scraper.py
@@ -214,6 +214,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# Extracted destinations code
-'''header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
