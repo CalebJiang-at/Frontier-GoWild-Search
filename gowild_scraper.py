207d92d20f8b240d0cc51eb28565cc28d9e0357d
Caleb Jiang <caleb.jiang@airtable.com>
2023-06-26 21:30:36 -0700

    cmd args

--- gowild_scraper.py
+++ gowild_scraper.py
@@ -2,6 +2,8 @@ import random, time, requests, json, html
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 import browsercookie
+import argparse
+
 
 # TODO 
 # Speed up processing time
@@ -30,8 +32,11 @@ destinations = {
     'PVR': 'Puerto Vallarta, MX', 
     'MTY': 'Monetrrey, MX', 
     'CUN': 'Cancun, MX', 
-    'CZM': 'Cozumel, MX', 
+    'CZM': 'Cozumel, MX',  
     'SXM': 'St. Maarten', 
+    'BQN': 'Aguadilla, Puerto Rico', 
+    'PSE': 'Ponce, Puerto Rico', 
+    'SJU': 'San Juan, Puerto Rico',
     'PHX': 'Phoenix', 
     'XNA': 'Arkansas', 
     'LIT': 'Little Rock, AR', 
@@ -88,9 +93,6 @@ destinations = {
     'MDT': 'Pennsylvania', 
     'PHL': 'Philadelphia', 
     'PIT': 'Pittsburgh', 
-    'BQN': 'Aguadilla, Puerto Rico', 
-    'PSE': 'Ponce, Puerto Rico', 
-    'SJU': 'San Juan, Puerto Rico', 
     'CHS': 'Charleston, South Carolina', 
     'MYR': 'Myrtle Beach, SC', 
     'TYS': 'Tennessee', 
@@ -110,7 +112,7 @@ destinations = {
     'MSN': 'Madison', 
     'MKE': 'Milwaukee'}
 
-def get_flight_html(origin, destinations, date, session):
+def get_flight_html(origin, destinations, date, session, cjs, start_index=0):
     user_agents = [
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
@@ -120,47 +122,50 @@ def get_flight_html(origin, destinations, date, session):
 
     f = open("destinations.txt", "a")
     f.write("Origin: " + origin + "\n")
-    for dest in destinations.keys():
+    destination_keys = list(destinations.keys()) # Retrieve a list of destination keys
+    for i in range(start_index, len(destination_keys)):
+        dest = destination_keys[i]
         # Choose a random User-Agent header
         header = {
             "User-Agent": random.choice(user_agents),
         }
-        cj = browsercookie.load()
+        cj = browsercookie.chrome() if cjs else None
 
-        time.sleep(random.uniform(0.5,1.5))
-        # Get schedule data for the route
-        schedule_url = f"https://booking.flyfrontier.com/Flight/RetrieveSchedule?calendarSelectableDays.Origin={origin}&calendarSelectableDays.Destination={dest}"
-        schedule_response = requests.Session().get(schedule_url, headers=header, cookies=cj)
-        
-        if schedule_response.status_code == 200:
-            schedule_data = schedule_response.json()
-            disabled_dates = schedule_data['calendarSelectableDays']['disabledDates']
-            last_available_date = schedule_data['calendarSelectableDays']['lastAvailableDate']
+        if cjs:
+            time.sleep(random.uniform(0.5,1.5))
+            # Get schedule data for the route
+            schedule_url = f"https://booking.flyfrontier.com/Flight/RetrieveSchedule?calendarSelectableDays.Origin={origin}&calendarSelectableDays.Destination={dest}"
+            schedule_response = requests.Session().get(schedule_url, headers=header, cookies=cj) if cjs else requests.Session().get(schedule_url, headers=header)
+            
+            if schedule_response.status_code == 200:
+                schedule_data = schedule_response.json()
+                disabled_dates = schedule_data['calendarSelectableDays']['disabledDates']
+                last_available_date = schedule_data['calendarSelectableDays']['lastAvailableDate']
 
-            # Convert the input date to the same format as the disabled dates list
-            formatted_date = datetime.strptime(date.replace('%20', ' '), '%b %d, %Y').strftime('%-m/%-d/%Y')
+                # Convert the input date to the same format as the disabled dates list
+                formatted_date = datetime.strptime(date.replace('%20', ' '), '%b %d, %Y').strftime('%-m/%-d/%Y')
 
-            # Check if the date is in the list of disabled dates
-            if formatted_date in disabled_dates or last_available_date == '0001-01-01 00:00:00':
-                print(f"No flights available on {formatted_date} from {origin} to {dest}. Date skipped.")
-                continue
-        else:
-            print(f"Problem accessing URL: code {schedule_response.status_code}\n url = " + schedule_url)
+                # Check if the date is in the list of disabled dates
+                if formatted_date in disabled_dates or last_available_date == '0001-01-01 00:00:00':
+                    print(f"{i}. No flights available on {formatted_date} from {origin} to {dest}. Date skipped.")
+                    continue
+            else:
+                print(f"{i}. Problem accessing URL: code {schedule_response.status_code}\n url = " + schedule_url)
 
         
         # Mimic human-like behavior by adding delays between requests
         delay = random.uniform(2, 5)  # Random delay between 2 to 5 seconds
         time.sleep(delay)
         url = f"https://booking.flyfrontier.com/Flight/InternalSelect?o1={origin}&d1={dest}&dd1={date}&ADT=1&mon=true&promo="
-        response = session.get(url, headers=header, cookies=cj)
+        response = session.get(url, headers=header, cookies=cj) if cjs else session.get(url, headers=header)
         if (response.status_code == 200):
             decoded_data = extract_html(response)
             if (decoded_data != 'NoneType'):
                 extract_json(decoded_data, origin, dest, date)
                 f.write(dest + ",")
         else:
-            print(f"Problem accessing URL: code {response.status_code}\n url = " + url)
-            continue
+            print(f"{i}. Problem accessing URL: code {response.status_code}\n url = " + url)
+            break
     f.close()
         
 def extract_json(flight_data, origin, dest, date):
@@ -211,16 +216,27 @@ def print_dests(origin):
 
 def main():
     global destinations
-    origin = input("Origin IATA airport code: ").upper()
-    input_dates = input("Show flights for:\n\tToday: 1\n\tTommorrow: 2\n\tBoth: 3\n\tTo Exit: 0\n")
+
+    parser = argparse.ArgumentParser(description='Check flight availability.')
+    parser.add_argument('-o', '--origin', type=str, required=True, help='Origin IATA airport code.')
+    parser.add_argument('-d', '--dates', type=str, required=True, help='Show flights for:\n\tToday: 1\n\tTommorrow: 2\n\tBoth: 3')
+    parser.add_argument('-c', '--cjs', action='store_true', help='Use browser cookies.')
+    parser.add_argument('-r', '--resume', type=int, default=0, help='Index of airport to resume from. Use index 21 to only search for contiguous US destinations.')
+
+    args = parser.parse_args()
+
+    origin = args.origin.upper()
+    input_dates = args.dates
+    cjs = args.cjs
     today = datetime.today()    
     session = requests.Session()
+    resume = args.resume
 
     if input_dates in ['1','3']:
         # Todays date in URL format
         travel_today = today.strftime("%b-%d,-%Y").replace("-", "%20")
         print("\nFlights for today:")
-        get_flight_html(origin, destinations, travel_today, session)
+        get_flight_html(origin, destinations, travel_today, session, cjs, resume)
         print_dests(origin)
 
     if input_dates in ['2','3']:
@@ -228,14 +244,8 @@ def main():
         tmrw = today + timedelta(days=1)
         travel_tmrw = tmrw.strftime("%b-%d,-%Y").replace("-", "%20")
         print("\nFlights for tommorrow:")
-        get_flight_html(origin, destinations, travel_tmrw, session)
+        get_flight_html(origin, destinations, travel_tmrw, session, cjs, resume)
         print_dests(origin)
-
-    if input_dates == '0':
-        return
-    else:
-        print("Retry")
-        main()
 
 if __name__ == "__main__":
     main()
